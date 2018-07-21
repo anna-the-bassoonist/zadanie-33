d01b64f5ba62d22e334db2befe389424c08fd00f
annafagocistka <anafagocistka@gmail.com>
2018-07-21 19:08:57 +0200

    proby

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var paragraf = document.getElementById('opis');
+
+var btn = document.querySelector('button');
+
+
+document.querySelector('button').onclick = function { 
+   console.log('Akademia 108');
+}
+    document.querySelector('button').onclick= ('Akademia 108');
