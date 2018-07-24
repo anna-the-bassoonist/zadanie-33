912f61764979ca76ea6c5a65e36b1a016e18b9e9
annafagocistka <anafagocistka@gmail.com>
2018-07-25 00:28:49 +0200

    odp

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var paragraf = document.getElementById('opis');
 var btn = document.querySelector('button');
 
 
-document.querySelector('button').onclick = function { 
-   console.log('Akademia 108');
+document.querySelector('button').onclick = function() { 
+paragraf.textContent = 'Akademia 108';
+    
 }
-    document.querySelector('button').onclick= ('Akademia 108');
